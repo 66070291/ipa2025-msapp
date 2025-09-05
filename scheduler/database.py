39034120f633123cf7a54be2614c4a803ddd9d33
Granvarden <premlekphon@gmail.com>
2025-09-05 16:47:40 +0700

    fix black format

--- scheduler/database.py
+++ scheduler/database.py
@@ -7,7 +7,6 @@ def get_router_info():
     mongo_uri = os.environ.get("MONGO_URI")
     db_name = os.environ.get("DB_NAME")
 
-
     client = MongoClient(mongo_uri)
     db = client[db_name]
     routers = db["routers"]
@@ -16,5 +15,5 @@ def get_router_info():
     return router_data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     get_router_info()
